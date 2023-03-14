c5d10d534b37195caea3ccac8dafbbdef31aa360
QuocAnh2001 <nguyenquocanh19122901@gmail.com>
2023-03-14 17:26:35 +0700

    register, login, get user, get all user

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/products");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(()=>console.log("DBconnect"))
+    .catch((err) => console.log(err));
+
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/products", productRoute);
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("BE server is running !");
+})
---
+++ models/Orders.js
@@ -0,0 +1,23 @@
+const mongoose = require ("mongoose");
+
+const OrderSchema =  new mongoose.Schema(
+{
+    userId: {type:String, required : true },
+    product: [
+        {
+            productId:{
+                type: String,
+            },
+            quantity:{
+                type: Number,
+                default: 1,
+            },
+        },
+    ],
+    amout: {type: Number, required: true},
+    address:{type: Object, required: true},
+    status: {type: String, default: "pending"},
+},
+    {timestamps: true}
+);
+module.exports = mongoose.model("Order",OrderSchema);
